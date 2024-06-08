9628ecbad54974bddb231884c69cd8621af3c472
SamLProgrammer <samuel.lopez@uptc.edu.co>
2024-06-07 20:25:40 -0500

    fixing monitor

--- controllers/monitor.js
+++ controllers/monitor.js
@@ -4,14 +4,14 @@ const formatCode = async (req, res) => {
     const { code } = req.body;
     console.log(code);
     try {
-        const formattedCode = await prettier.format(htmlString, { parser: "html" });
+        const formattedCode = await prettier.format(code, { parser: "html" });
         console.log(formattedCode);
         res.json({ formattedCode });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
-
 }
+
 
 
 module.exports = {
