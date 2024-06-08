1c14058667a4ec6fec3c5b6382dcf1f44ce6441e
SamLProgrammer <samuel.lopez@uptc.edu.co>
2024-06-07 19:37:49 -0500

    adding eslint

--- controllers/monitor.js
+++ controllers/monitor.js
@@ -1,17 +1,31 @@
 const prettier = require('prettier');
+const eslint = require('eslint');
+const prettierEslint = require('prettier-eslint');
+
+// const formatCode = async (req, res) => {
+//     const { code } = req.body;
+//     console.log(code);
+//     try {
+//         const escapedCode = code.replace(/\n/g, '\\n');
+//         const formattedCode = await prettier.format(code, { parser: 'babel' });
+//         console.log(formattedCode);
+//         res.json({ formattedCode });
+//     } catch (error) {
+//         res.status(500).json({ error: error.message });
+//     }
+
+// }
 
 const formatCode = async (req, res) => {
     const { code } = req.body;
-    console.log(code);
+    console.log(code); // Check the code before formatting
     try {
-        // Format the code using Prettier
-        const formattedCode = await prettier.format(code, { parser: 'babel' });
-        console.log(formattedCode);
+        const formattedCode = await prettierEslint.format(code);
+        console.log(formattedCode); // Check the formatted code
         res.json({ formattedCode });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
-
 }
 
 module.exports = {
