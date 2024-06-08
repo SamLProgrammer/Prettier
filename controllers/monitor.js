44f3274b13e342b302dc92b6c69159c7ad4b8434
SamLProgrammer <samuel.lopez@uptc.edu.co>
2024-06-07 20:00:52 -0500

    eslint removed

--- controllers/monitor.js
+++ controllers/monitor.js
@@ -1,32 +1,18 @@
 const prettier = require('prettier');
-const eslint = require('eslint');
-const prettierEslint = require('prettier-eslint');
-
-// const formatCode = async (req, res) => {
-//     const { code } = req.body;
-//     console.log(code);
-//     try {
-//         const escapedCode = code.replace(/\n/g, '\\n');
-//         const formattedCode = await prettier.format(code, { parser: 'babel' });
-//         console.log(formattedCode);
-//         res.json({ formattedCode });
-//     } catch (error) {
-//         res.status(500).json({ error: error.message });
-//     }
-
-// }
 
 const formatCode = async (req, res) => {
     const { code } = req.body;
-    console.log(code); // Check the code before formatting
+    console.log(code);
     try {
-        const formattedCode = await prettierEslint.format(code);
-        console.log(formattedCode); // Check the formatted code
+        const formattedCode = await prettier.format(htmlString, { parser: "html" });
+        console.log(formattedCode);
         res.json({ formattedCode });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
+
 }
+
 
 module.exports = {
     formatCode,
