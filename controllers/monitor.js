d40e9bdde7fb0b37ee8e1b9ab438d8c05a3eb3f7
SamLProgrammer <samuel.lopez@uptc.edu.co>
2024-06-11 09:22:26 -0500

    working version with LWC formatter

--- controllers/monitor.js
+++ controllers/monitor.js
@@ -2,15 +2,20 @@ const prettier = require('prettier');
 
 const formatCode = async (req, res) => {
     const { code } = req.body;
-    console.log(code);
+
+    // Apply replacements directly on the code string
+    const codeToFormat = code.replace(/\s+/g, ' ').replace(/(["'])/g, '\\$1');
+
     try {
-        const formattedCode = await prettier.format(code, { parser: "babel" });
+        const formattedCode = await prettier.format(codeToFormat, { parser: "lwc" });
         console.log(formattedCode);
         res.json({ formattedCode });
     } catch (error) {
+        console.log(error.message);
         res.status(500).json({ error: error.message });
     }
 }
+
 
 
 
