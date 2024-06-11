2982ebd40b157eb9710d3fb7fab905868ccd4242
SamLProgrammer <samuel.lopez@uptc.edu.co>
2024-06-11 10:38:39 -0500

    setting it back to babel

--- controllers/monitor.js
+++ controllers/monitor.js
@@ -7,7 +7,7 @@ const formatCode = async (req, res) => {
     // const codeToFormat = code.replace(/\s+/g, ' ').replace(/(["'])/g, '\\$1');
 
     try {
-        const formattedCode = await prettier.format(code, { parser: "yaml" });
+        const formattedCode = await prettier.format(code, { parser: "babel" });
         console.log(formattedCode);
         res.json({ formattedCode });
     } catch (error) {
